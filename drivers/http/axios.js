ee3ec08adcbee2b04713b5f8e28322bc20e6fba6
mubaidr <mubaidr@gmail.com>
2017-07-07 15:13:41 +0500

    use installed vue-resource

--- drivers/http/axios.js
+++ drivers/http/axios.js
@@ -1,13 +1,13 @@
-let axios = require('axios')
+let $axios = require('axios')
 
 module.exports = {
   _init: function (Vue) {
-    if (!axios) {
+    if (!$axios) {
       console.warn('@deveodk/vue-error-tracker : axios must be set before this plugin is used.')
     }
   },
   _interceptor: function (Vue, code, errorCb) {
-    if (axios) {
+    if ($axios) {
       axios.interceptors.response.use(function (response) {
         code(response)
         return response

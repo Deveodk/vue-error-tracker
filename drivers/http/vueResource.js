2bddb5a428a2c8642e6407c4ed6e80f2ad3c68f4
mubaidr <mubaidr@gmail.com>
2017-07-07 15:17:37 +0500

    revert vue-resource change

--- drivers/http/vueResource.js
+++ drivers/http/vueResource.js
@@ -1,11 +1,11 @@
 module.exports = {
   _init: function (Vue) {
-    if (!Vue.http) {
+    if (!Vue.prototype.http) {
       console.warn('@deveodk/vue-error-tracker : vue-resource must be set before this plugin is used.')
     }
   },
   _interceptor: function (Vue, code) {
-    Vue.http.interceptors.push(function (request, next) {
+    Vue.prototype.http.interceptors.push(function (request, next) {
       next(function (response) {
         code(response)
       })
